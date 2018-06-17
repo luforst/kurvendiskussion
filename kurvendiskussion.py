444cc1a0691a735b01c412659fb0d4947b453bdf
Lukas Forst <lukas.forst@zoho.com>
2018-06-17 14:14:39 +0200

    Add basic input functionality and calculation of derivatives

--- kurvendiskussion.py
+++ kurvendiskussion.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+import sympy as sym
 
 # Eingabe der Funktion
-
+funktion_str = input("Funktion eingeben: f(x)=")
+f = sym.Symbol('(' + funktion_str + ')')
 
 # Bestimmung der Ableitungen
-
+x = sym.Symbol('x')
+f1 = sym.diff(f, x, 1)
+f2 = sym.diff(f, x, 2)
+f3 = sym.diff(f, x, 3)
 
 # Nullstellen
 
