d47820676fa62f28ae6744545a57f2ebd8aa66d6
Lukas Forst <lukas.forst@zoho.com>
2018-06-22 16:23:42 +0200

    Make function insert/load work

--- kurvendiskussion.py
+++ kurvendiskussion.py
@@ -3,10 +3,11 @@ import sympy as sym
 
 # Eingabe der Funktion
 funktion_str = input("Funktion eingeben: f(x)=")
-f = sym.Symbol('(' + funktion_str + ')')
+f = sym.Symbol('f')
+x = sym.Symbol('x')
+f = eval(funktion_str)
 
 # Bestimmung der Ableitungen
-x = sym.Symbol('x')
 f1 = sym.diff(f, x, 1)
 f2 = sym.diff(f, x, 2)
 f3 = sym.diff(f, x, 3)
