341af056236c81eeb42cf4aaa1a500ba4e600759
Lukas Forst <lukas.forst@zoho.com>
2018-08-06 11:49:04 +0200

    Restructure program into functions

--- kurvendiskussion.py
+++ kurvendiskussion.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
 import sympy as sym
 
-# Eingabe der Funktion
-funktion_str = input("Funktion eingeben: f(x)=")
-f = sym.Symbol('f')
-x = sym.Symbol('x')
-f = eval(funktion_str)
+global x
+global f
 
-# Bestimmung der Ableitungen
-f1 = sym.diff(f, x, 1)
-f2 = sym.diff(f, x, 2)
-f3 = sym.diff(f, x, 3)
-
-# Nullstellen
-nullstellen = sym.solveset(f, x)
+def ableitg123(f):
+	global f1 = sym.diff(f, x, 1)
+	global f2 = sym.diff(f, x, 2)
+	global f3 = sym.diff(f, x, 3)
 
 
-# Extremstellen / -punkte
-extremstellen_mögl = list(sym.solveset(f1, x)) # Bestimme mögliche Extremstellen anhand notwendiger Bedingung f'(x)=0
-extremstellen = {x0 for x0 in extremstellen_mögl if x0 not in sym.solveset(f2, x)}
+def nullst(f):
+	nullstellen = sym.solveset(f, x)
+	return nullstellen
 
 
-# Wendestellen / -punkte
-wendestellen_mögl = list(sym.solveset(f2, x)) # mögl. Wendest. anhand notwendiger Bedingung f''(x)=0
-wendestellen = {x0 for x0 in wendestellen_mögl if x0 not in sym.solveset(f3, x)}
+def extremst(f):
+	# Bestimme mögliche Extremstellen anhand notwendiger Bedingung f'(x) == 0
+	extremstellen_mögl = list(sym.solveset(f1, x))
+	# Bestimme tatsächliche Extremstellen anhand hinreichender Bedingung f''(x) != 0
+	extremstellen = {x0 for x0 in extremstellen_mögl if x0 not in sym.solveset(f2, x)}
+	return extremstellen
+
+
+def wendest(f):
+	# Bestimme mögliche Wendestellen anhand notwendiger Bedingung f''(x)=0
+	wendestellen_mögl = list(sym.solveset(f2, x))
+	# Bestimme tatsächliche Wendestellen anhand hinreichender Bedingung f'''(x) != 0
+	wendestellen = {x0 for x0 in wendestellen_mögl if x0 not in sym.solveset(f3, x)}
+	return wendestellen
+
+
+def main():
+	funktion_str = input("Funktion eingeben: f(x)=")
+	x = sym.Symbol('x')
+	f = eval(funktion_str)
